4d53849eef10b176c1418c61de6304706e081a06
Andrea Velasquez <velasquez189@gmail.com>
2018-07-14 09:35:33 -0400

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 )
